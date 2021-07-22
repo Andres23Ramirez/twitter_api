1c46a3b38fabe3133ae43443c6d52cb945829261
rodrigo23ramirez <rodrigo23ramirez@gmail.com>
2021-07-22 23:34:46 +0100

    Python file to download tweets from api twitter

---
+++ search.py
@@ -0,0 +1,50 @@
+# Import the Twython class
+from twython import Twython
+import json
+import pandas as pd
+import csv
+
+# Load credentials from json file
+with open("twitter_credentials.json", "r") as file:
+    creds = json.load(file)
+
+# Instantiate an object
+python_tweets = Twython(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
+
+# Create our query
+query = {'q': 'vacuna',
+        'result_type': 'mixed',
+        'count': 100,
+        'lang': 'es',
+        'geocode': {"4.6762539246186385", "-74.11467451254052","50km" }
+        }
+
+statuses = python_tweets.search(**query)['statuses']
+
+
+mayor = 0
+index = 0
+for x in statuses:
+    if len(x) > mayor:
+        mayor = len(x)
+        index = statuses.index(x)
+
+#myheaders = statuses[index].keys()
+myheaders = ['extended_entities', 'retweeted_status', 'created_at', 'id', 'id_str', 'text', 'truncated', 'entities', 'metadata', 'source', 'in_reply_to_status_id', 'in_reply_to_status_id_str', 'in_reply_to_user_id', 'in_reply_to_user_id_str', 'in_reply_to_screen_name', 'user', 'geo', 'coordinates', 'place', 'contributors', 'is_quote_status', 'quoted_status_id', 'quoted_status_id_str', 'quoted_status', 'retweet_count', 'favorite_count', 'favorited', 'retweeted', 'possibly_sensitive', 'lang']
+
+with open('tweets_result1.csv', 'w', newline='') as myfile:
+    writer = csv.DictWriter(myfile, fieldnames=myheaders)
+    writer.writeheader()
+    writer.writerows(statuses)  
+# Search tweets
+""" dict_ = {'user': [], 'date': [], 'text': [], 'favorite_count': []}
+for status in python_tweets.search(**query)['statuses']:
+    dict_['user'].append(status['user']['screen_name'])
+    dict_['date'].append(status['created_at'])
+    dict_['text'].append(status['text'])
+    dict_['favorite_count'].append(status['favorite_count'])
+
+# Structure data in a pandas DataFrame for easier manipulation
+df = pd.DataFrame(dict_)
+df.sort_values(by='favorite_count', inplace=True, ascending=False)
+print(df) """
