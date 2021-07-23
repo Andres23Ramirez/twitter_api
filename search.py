a0a442e437667e01f8a4a5bc2482a1d648f47f2f
rodrigo23ramirez <rodrigo23ramirez@gmail.com>
2021-07-23 02:42:43 +0100

    Fixed full text tweets

--- search.py
+++ search.py
@@ -12,15 +12,15 @@ with open("twitter_credentials.json", "r") as file:
 python_tweets = Twython(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
 
 # Create our query
-query = {'q': 'pfizer',
+query = {'q': 'pfizer ',
         'result_type': 'mixed',
         'count': 100,
         'lang': 'es',
-        'geocode': {"4.6762539246186385", "-74.11467451254052","50km" }
+        'geocode': {"4.6762539246186385", "-74.11467451254052","50km" },
+        'tweet_mode': 'extended'
         }
 
 statuses = python_tweets.search(**query)['statuses']
-
 
 mayor = 0
 index = 0
@@ -30,12 +30,13 @@ for x in statuses:
         index = statuses.index(x)
 
 #myheaders = statuses[index].keys()
-myheaders = ['extended_entities', 'retweeted_status', 'created_at', 'id', 'id_str', 'text', 'truncated', 'entities', 'metadata', 'source', 'in_reply_to_status_id', 'in_reply_to_status_id_str', 'in_reply_to_user_id', 'in_reply_to_user_id_str', 'in_reply_to_screen_name', 'user', 'geo', 'coordinates', 'place', 'contributors', 'is_quote_status', 'quoted_status_id', 'quoted_status_id_str', 'quoted_status', 'retweet_count', 'favorite_count', 'favorited', 'retweeted', 'possibly_sensitive', 'lang']
+#myheaders = ['extended_entities', 'retweeted_status', 'created_at', 'id', 'id_str','full_text', 'text', 'truncated', 'entities', 'metadata', 'source', 'in_reply_to_status_id', 'in_reply_to_status_id_str', 'in_reply_to_user_id', 'in_reply_to_user_id_str', 'in_reply_to_screen_name', 'user', 'geo', 'coordinates', 'place', 'contributors', 'is_quote_status', 'quoted_status_id', 'quoted_status_id_str', 'quoted_status', 'retweet_count', 'favorite_count', 'favorited', 'retweeted', 'possibly_sensitive', 'lang']
+myheaders = ['created_at', 'id', 'id_str','full_text', 'source','truncated','in_reply_to_status_id','in_reply_to_status_id_str','in_reply_to_user_id','in_reply_to_user_id_str','in_reply_to_screen_name','user','coordinates','place','quoted_status_id','quoted_status_id_str','is_quote_status','quoted_status','retweeted_status','quote_count','reply_count','retweet_count','favorite_count','entities','extended_entities','favorited','retweeted','possibly_sensitive','filter_level','lang','matching_rules', 'current_user_retweet', 'scopes', 'withheld_copyright', 'withheld_in_countries', 'withheld_scope', 'contributors', 'geo', 'metadata', 'display_text_range']
 
-with open('tweets_result2.csv', 'w', newline='') as myfile:
+with open('tweets_result2.csv', 'w', newline='') as myfile:    
     writer = csv.DictWriter(myfile, fieldnames=myheaders)
     writer.writeheader()
-    writer.writerows(statuses)  
+    writer.writerows(statuses) 
 # Search tweets
 """ dict_ = {'user': [], 'date': [], 'text': [], 'favorite_count': []}
 for status in python_tweets.search(**query)['statuses']:
