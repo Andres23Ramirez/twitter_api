a41897a804e9a1cecfeca5c603a97c41a0704308
rodrigo23ramirez <rodrigo23ramirez@gmail.com>
2021-07-24 03:10:18 +0100

    In this commit is added: - Download tweets by passing a list of words - Increased search radius to 700km - Get the full text of the tweet, RTs are cut by the api - All downloads are unified in a single file called: tweets.csv - It is configured to get the most out of the API, 10,000 tweets are downloaded in 17 minutes. - The download is parameterized by tweet id to avoid repeated tweets.

--- search.py
+++ search.py
@@ -3,6 +3,8 @@ from twython import Twython
 import json
 import pandas as pd
 import csv
+import os.path
+import time
 
 # Load credentials from json file
 with open("twitter_credentials.json", "r") as file:
@@ -10,33 +12,53 @@ with open("twitter_credentials.json", "r") as file:
 
 # Instantiate an object
 python_tweets = Twython(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
+words = ["pfizer", "sinovac", "astrazenea", "janssen", "vacuna"]
+query_word = " OR ".join(words)
+min = 0
 
 # Create our query
-query = {'q': 'pfizer ',
+query = {'q': query_word,
         'result_type': 'mixed',
         'count': 100,
         'lang': 'es',
-        'geocode': {"4.6762539246186385", "-74.11467451254052","50km" },
+        'geocode': {"4.6762539246186385", "-74.11467451254052","700km" },
         'tweet_mode': 'extended'
         }
 
 statuses = python_tweets.search(**query)['statuses']
 
-mayor = 0
-index = 0
-for x in statuses:
-    if len(x) > mayor:
-        mayor = len(x)
-        index = statuses.index(x)
-
-#myheaders = statuses[index].keys()
-#myheaders = ['extended_entities', 'retweeted_status', 'created_at', 'id', 'id_str','full_text', 'text', 'truncated', 'entities', 'metadata', 'source', 'in_reply_to_status_id', 'in_reply_to_status_id_str', 'in_reply_to_user_id', 'in_reply_to_user_id_str', 'in_reply_to_screen_name', 'user', 'geo', 'coordinates', 'place', 'contributors', 'is_quote_status', 'quoted_status_id', 'quoted_status_id_str', 'quoted_status', 'retweet_count', 'favorite_count', 'favorited', 'retweeted', 'possibly_sensitive', 'lang']
 myheaders = ['created_at', 'id', 'id_str','full_text', 'source','truncated','in_reply_to_status_id','in_reply_to_status_id_str','in_reply_to_user_id','in_reply_to_user_id_str','in_reply_to_screen_name','user','coordinates','place','quoted_status_id','quoted_status_id_str','is_quote_status','quoted_status','retweeted_status','quote_count','reply_count','retweet_count','favorite_count','entities','extended_entities','favorited','retweeted','possibly_sensitive','filter_level','lang','matching_rules', 'current_user_retweet', 'scopes', 'withheld_copyright', 'withheld_in_countries', 'withheld_scope', 'contributors', 'geo', 'metadata', 'display_text_range']
 
-with open('tweets_result2.csv', 'w', newline='') as myfile:    
-    writer = csv.DictWriter(myfile, fieldnames=myheaders)
-    writer.writeheader()
-    writer.writerows(statuses) 
+if os.path.isfile('tweets.csv'):
+    exits = True
+else:
+    exits = False
+
+for i in range(0, 100, 1):
+    with open('tweets.csv', 'a', newline='') as myfile:    
+        writer = csv.DictWriter(myfile, fieldnames=myheaders)
+        if not exits:
+            writer.writeheader()
+        writer.writerows(statuses) 
+
+    for status in statuses:
+        if min < status['id']:
+            min = status['id']
+    
+    query = {'q': query_word,
+        'result_type': 'mixed',
+        'count': 100,
+        'lang': 'es',
+        'geocode': {"4.6762539246186385", "-74.11467451254052","700km" },
+        'tweet_mode': 'extended',
+        'max_id': min
+        }
+    
+    time.sleep(10)
+    
+ 
+print(min)
+
 # Search tweets
 """ dict_ = {'user': [], 'date': [], 'text': [], 'favorite_count': []}
 for status in python_tweets.search(**query)['statuses']:
