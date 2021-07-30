4093fd474a554c063d6e57d295bc7b1e87a6bfd2
Andres23Ramirez <rodrigo23ramirez@gmail.com>
2021-07-30 21:16:52 +0100

    Creted search users and improved search tweets

--- search_tweets.py
+++ search_tweets.py
@@ -12,61 +12,69 @@ with open("twitter_credentials.json", "r") as file:
 
 # Instantiate an object
 python_tweets = Twython(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
-words = ["pfizer", "sinovac", "astrazenea", "janssen", "vacuna"]
+words = ["pfizer", "sinovac", "astrazenea", "janssen", "vacuna", "vacunacion"]
 query_word = " OR ".join(words)
-min = 0
+query_word = "Colombia AND " + query_word
+min = 99999999999999999999
+#min = 0
+
 
 # Create our query
-query = {'q': query_word,
+myheaders = ['created_at', 'id', 'id_str','full_text', 'source','truncated','in_reply_to_status_id','in_reply_to_status_id_str','in_reply_to_user_id','in_reply_to_user_id_str','in_reply_to_screen_name','user','coordinates','place','quoted_status_id','quoted_status_id_str','is_quote_status','quoted_status','retweeted_status','quote_count','reply_count','retweet_count','favorite_count','entities','extended_entities','favorited','retweeted','possibly_sensitive','filter_level','lang','matching_rules', 'current_user_retweet', 'scopes', 'withheld_copyright', 'withheld_in_countries', 'withheld_scope', 'contributors', 'geo', 'metadata', 'display_text_range']
+
+if os.path.isfile('tweets_mejorado.csv'):
+    exits = True
+    df = pd.read_csv('tweets_mejorado.csv')
+    min = df['id'].min()
+    min = min - 1
+    print("File exist: ")
+    print(min)
+    query = {'q': query_word,
         'result_type': 'mixed',
         'count': 100,
         'lang': 'es',
-        'geocode': {"4.6762539246186385", "-74.11467451254052","700km" },
+        'tweet_mode': 'extended',
+        'max_id': min
+        }    
+else:
+    exits = False
+    query = {'q': query_word,
+        'result_type': 'mixed', 
+        'count': 100,
+        'lang': 'es',
         'tweet_mode': 'extended'
         }
 
-myheaders = ['created_at', 'id', 'id_str','full_text', 'source','truncated','in_reply_to_status_id','in_reply_to_status_id_str','in_reply_to_user_id','in_reply_to_user_id_str','in_reply_to_screen_name','user','coordinates','place','quoted_status_id','quoted_status_id_str','is_quote_status','quoted_status','retweeted_status','quote_count','reply_count','retweet_count','favorite_count','entities','extended_entities','favorited','retweeted','possibly_sensitive','filter_level','lang','matching_rules', 'current_user_retweet', 'scopes', 'withheld_copyright', 'withheld_in_countries', 'withheld_scope', 'contributors', 'geo', 'metadata', 'display_text_range']
-
-if os.path.isfile('tweets.csv'):
-    exits = True
-else:
-    exits = False
-
 for i in range(0, 100, 1):
-
+    print(i)
+    print("-------------------------------------------")
+    print("Query: " + str(query))
     statuses = python_tweets.search(**query)['statuses']
-
-    with open('tweets.csv', 'a', newline='') as myfile:    
+    print("Len: " + str(len(statuses)))
+    time.sleep(10)
+    with open('tweets_mejorado.csv', 'a', newline='') as myfile:    
         writer = csv.DictWriter(myfile, fieldnames=myheaders)
         if not exits:
             writer.writeheader()
+            exits = True
         writer.writerows(statuses) 
 
-    for status in statuses:
-        if min < status['id']:
+    for status in statuses:     
+        if min + 1 > status['id']:
+            print("cambiando")
+            print("min:    " + str(min))
+            print("Status: " + str(status['id']))
+            print("Resta: " + str(status['id'] - min))  
             min = status['id']
+                    
+            
+    print("For min: ")
+    print(min)
     
     query = {'q': query_word,
         'result_type': 'mixed',
         'count': 100,
         'lang': 'es',
-        'geocode': {"4.6762539246186385", "-74.11467451254052","700km" },
         'tweet_mode': 'extended',
         'max_id': min
         }
-    
-    time.sleep(10)
-    print(i)
-
-# Search tweets
-""" dict_ = {'user': [], 'date': [], 'text': [], 'favorite_count': []}
-for status in python_tweets.search(**query)['statuses']:
-    dict_['user'].append(status['user']['screen_name'])
-    dict_['date'].append(status['created_at'])
-    dict_['text'].append(status['text'])
-    dict_['favorite_count'].append(status['favorite_count'])
-
-# Structure data in a pandas DataFrame for easier manipulation
-df = pd.DataFrame(dict_)
-df.sort_values(by='favorite_count', inplace=True, ascending=False)
-print(df) """
