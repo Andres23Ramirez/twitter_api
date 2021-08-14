82a836eb4c4886787aabfb667ccb9842a899fa74
Andres23Ramirez <rodrigo23ramirez@gmail.com>
2021-08-14 19:21:45 +0100

    Created pivotable in EDA

--- views/EDA.py
+++ views/EDA.py
@@ -1,27 +1,30 @@
+import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 from views import commonmodules
 import pandas as pd
-df = pd.read_csv('data/tweets.csv')
+import dash_pivottable
+
+df = pd.read_csv('data/tweets_mejorado 2.csv')
+df = df.filter(["id","created_at", "coordinates", "place", "quote_count", "reply_count", "retweet_count",	"favorite_count",
+	            "favorited", "retweeted",	"possibly_sensitive",	"filter_level",	"current_user_retweet", "display_text_range"])
 
 from app import app
+df2 = df[:5]
+print(len(df2))
 
 layout = html.Div([
     commonmodules.get_header(),
     commonmodules.get_menu(),   
-    html.H3('Line Chart'),    
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': df['created_at'], 'y': df['favorite_count'], 'type': 'line', 'name': 'Tweets Favorites'},
-                {'x': df['created_at'], 'y': df['retweet_count'], 'type': 'line', 'name': 'Retweets'}
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
+    html.Div(
+        dash_pivottable.PivotTable(
+            data= df.values.tolist(),
+            cols=["reply_count"],
+            rows=df2["id"],
+            vals=["Count"]
+        ), 
+        className='container'
     ),
     commonmodules.get_footer(),   
 ])
