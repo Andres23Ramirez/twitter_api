e16b33e333d6f24a3a17918562b280812f260236
Andres23Ramirez <rodrigo23ramirez@gmail.com>
2021-09-07 04:47:44 +0100

    Changes in the styles

--- index.py
+++ index.py
@@ -28,4 +28,4 @@ def display_page(pathname):
 
 # Start the server
 if __name__ == '__main__':
-    app.run_server(host="0.0.0.0", port="8050", debug=True)
+    app.run_server(host="0.0.0.0", port="8050", debug=False)
